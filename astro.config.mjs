5a77058b14066e000a8139c27ab54e9ecc08498f
harinallaparaju <hari.nallaparaju@gmail.com>
2025-08-11 16:31:43 +0530

    fix: Add React dependencies for Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://index.astrojs.org",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
   output: "static",
   adapter: vercel(),
 });
