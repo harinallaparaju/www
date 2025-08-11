cb3c3943e01c577efdb0fdddae241eb30629e97f
harinallaparaju <hari.nallaparaju@gmail.com>
2025-08-11 11:55:53 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://index.astrojs.org",
+  integrations: [tailwind(), sitemap()],
+  output: "static",
+  adapter: vercel(),
+});
