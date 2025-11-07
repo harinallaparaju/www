a93ab78524207ecf679f77cebbfc58159cc85fbc
harinallaparaju <hari.nallaparaju@gmail.com>
2025-11-08 00:14:37 +0530

    made changes to the astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/static";
 import react from "@astrojs/react";
-import rss from '@astrojs/rss';
 
 export default defineConfig({
   site: "https://suryanallaparaju.com",
-  integrations: [tailwind(), sitemap(), react(), vercel()],
+  integrations: [tailwind(), sitemap(), react()],
   output: "static",
   adapter: vercel(),
 });
